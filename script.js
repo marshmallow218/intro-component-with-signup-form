f4fd6f97fd849b9adb54bb26ff73f4c0f98c1121
Kurt Renzo Jalalain <jalalainkurt@gmail.com>
2023-10-22 14:07:08 +0800

    js validation

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
-const btn = document.querySelector('.btn');
+const form = document.querySelector('form');
 const firstName = document.querySelector('#first-name');
 const lastName = document.querySelector('#last-name');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 
 
-const setError = (element, message) => {
-    const inputBox = element.parentElement;
-    const errorDisplay = inputBox.querySelector('.error-msg');
+const setError = (input, message) => {
+    const inputBox = input.parentElement;
     const errorIcon = inputBox.querySelector('img');
+    const errorMsg = inputBox.querySelector('span');
+    
+    errorIcon.style.display = 'block';
+    errorMsg.innerText = message;
+    errorMsg.style.display = 'block';
+    input.classList.add('error');
+    input.classList.remove('success')
+}
 
-    errorIcon.style.display = 'block';   
-    errorDisplay.style.display = 'block';
-    errorDisplay.innerText = message;
-    element.classList.add('error');
-    element.classList.remove('success');
-};
-
-const setSuccess = element => {
-    const inputBox = element.parentElement;
-    const errorDisplay = inputBox.querySelector('.error-msg');
+const setSuccess = input => {
+    const inputBox = input.parentElement;
     const errorIcon = inputBox.querySelector('img');
+    const errorMsg = inputBox.querySelector('span');
+    
+    errorIcon.style.display = 'none';     
+    errorMsg.style.display = 'none';
+    input.classList.add('success');
+    input.classList.remove('error')
+}
 
-    errorIcon.style.display = 'none';
-    errorDisplay.style.display = 'none';
-    element.classList.add('success');
-    element.classList.remove('error');
-};
 
-const validateInputs = () => {
-    const firstNameValue = firstName.value;
-    const lastNameValue = lastName.value;
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    validateInputs();
+})
+
+
+function validateInputs() {
+    const firstNameValue = firstName.value.trim();
+    const lastNameValue = lastName.value.trim();
     const emailValue = email.value;
     const passwordValue = password.value;
     
@@ -52,19 +60,12 @@ const validateInputs = () => {
         setSuccess(email);
     }
     
-    if (passwordValue.length === 0) {
+    if (passwordValue.length <= 0) {
         setError(password, "Password cannot be empty");
-    } else if (passwordValue.length <= 8) {
-        setError(password, "Password must be longer than 8 characters");
+    } else if (passwordValue.length < 8) {
+        setError(password, "Password cannot be less than 8 characters");
     } else {
         setSuccess(password);
     }
-};
+}
 
-
-btn.addEventListener('click', e => {                
-    e.preventDefault();
-    
-    validateInputs();                                                              
-});                
-    
